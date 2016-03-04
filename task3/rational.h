b2ee244028fd499128cfcfa7ab55821f4bf0822d
Killaz <Kanootoko@gmail.com>
2016-03-04 23:17:22 +0300

    task3: plus-minus INF cases solved

--- task3/rational.h
+++ task3/rational.h
@@ -1,12 +1,15 @@
+#ifndef _RATIONAL_H
+#define _RATIONAL_H
+
 typedef long long lld;
 
 class rational {
 	private:
-	int q, v;     // r = q / v
+	int q, v;     // r = q / v ;   v = 0 => +-inf ;   v < 0 => invalid
 	static int gcd(int a, int b) {
 		int sgn = 1;
 		if (a < 0)
-			sgn = -sgn, a = -a;
+			sgn = -1, a = -a;
 		if (b < 0)
 			sgn = -sgn, b = -b;
 		while (a != 0 && b != 0)
@@ -19,10 +22,13 @@ class rational {
 	static int lcm(int a, int b) {
 		return (int) ((lld) a * b / gcd(a, b));
 	}
-	static void swap(int *a, int *b) {
-		int c = *a;
-		*a = *b;
-		*b = c;
+	static int sign(int a) {
+		return (a > 0) ? 1 : ((a == 0) ? 0 : -1);
+	}
+	static rational unsafeCreate(int q, int v) {
+		rational ans;
+		ans.q = q, ans.v = v;
+		return ans;
 	}
 	public:
 	rational(int i) {
@@ -45,13 +51,19 @@ class rational {
 		return rational(this->v, this->q);
 	}
 	void roll() {
-		swap(&this->q, &this->v);
+		*this = this->rollV();
 	}
 	rational operator-() {
 		return rational(-this->q, this->v);
 	}
 	rational operator* (rational r) {
 		rational ans;
+		if (this->v < 0 || r.v < 0)
+			return unsafeCreate(1, -1);
+		if ((this->v == 0 && r.q == 0) || (this->q == 0 && r.v == 0))
+			return unsafeCreate(1, -1);
+		if (this->v == 0 || r.v == 0)
+			return unsafeCreate(sign(this->q) * sign(r.q), 0);
 		int gcd1 = gcd(this->q, r.v), gcd2 = gcd(r.q, this->v);
 		ans.q = (this->q / gcd1) * (r.q / gcd2);
 		ans.v = (r.v / gcd1) * (this->v / gcd2);
@@ -61,6 +73,16 @@ class rational {
 		return *this * r.rollV();
 	}
 	rational operator+ (rational r) {
+		if (this->v < 0 || r.v < 0)
+			return unsafeCreate(1, -1);
+		if ((this->v == 0) ^ (r.v == 0))
+			return unsafeCreate(sign((this->v == 0) ? this->q : r.q), 0);
+		if (this->v == 0 && r.v == 0) {
+			if (sign(this->v) == sign(r.v))
+				return unsafeCreate(sign(this->v), 0);
+			else
+				return unsafeCreate(1, -1);
+		}
 		lld gcdV = gcd(this->v, r.v);
 		lld q = (lld) this->q * r.v / gcdV + (lld) r.q * this->v / gcdV;
 		lld v = (lld) this->v * r.v / gcdV;
@@ -96,3 +118,5 @@ class rational {
 		return this->v;
 	}
 };
+
+#endif
